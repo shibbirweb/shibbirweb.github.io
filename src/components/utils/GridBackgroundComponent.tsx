5d70fbb33d20e5c4eb546723a63edb5430540466
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2025-06-02 03:01:35 +0600

    closes #19. Implement grid background to top section

---
+++ src/components/utils/GridBackgroundComponent.tsx
@@ -0,0 +1,5 @@
+export default function GridBackgroundComponent() {
+    return (
+        <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_80%,transparent_100%)] bg-[size:20px_20px]"></div>
+    );
+}
