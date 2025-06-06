509389241116551357ce9f9451917bad546be00d
MD. Shibbir Ahmed <shibbir.ahmed@robendevs.com>
2025-06-06 09:58:00 +0600

    fix: #26: Add aria-label into the links

--- src/app/_root/hero-area/SocialIcons.tsx
+++ src/app/_root/hero-area/SocialIcons.tsx
@@ -4,18 +4,23 @@ import { socialLinks } from './contents';
 export default function SocialIcons() {
     return (
         <div className="flex flex-wrap items-center justify-center gap-2 select-none sm:gap-4">
-            {socialLinks.map(({ Icon, href, socialColorClassNames }) => (
+            {socialLinks.map(({ name, Icon, href, socialColorClassNames }) => (
                 <a
                     key={href}
                     href={href}
                     target="_blank"
                     rel="noopener noreferrer"
+                    title={name}
                     className={cn(
                         'p-2 transition-all duration-300 hover:scale-110',
                         socialColorClassNames
                     )}
                 >
-                    <Icon className="size-5 sm:size-6" />
+                    <Icon
+                        className="size-5 sm:size-6"
+                        aria-label={name}
+                        aria-hidden="true"
+                    />
                 </a>
             ))}
         </div>
