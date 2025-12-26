0e302b50be9e6817d5962e3ee1893296042fc560
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2025-12-27 03:28:49 +0600

    feat: Add bottom SVG Logo
    Closes #38

---
+++ src/app/_root/footer-area/index.tsx
@@ -0,0 +1,11 @@
+import FooterBottom from '@/components/pages/common/FooterBottom';
+
+export default function FooterArea() {
+    return (
+        <footer className="flex flex-col pt-8">
+            <div className="container mx-auto flex grow flex-col px-4">
+                <FooterBottom />
+            </div>
+        </footer>
+    );
+}
---
+++ src/components/pages/common/FooterBottom.tsx
@@ -0,0 +1,9 @@
+import Shibbir from '@/components/icons/shibbir';
+
+export default function FooterBottom() {
+    return (
+        <div>
+            <Shibbir className="text-neutral-300 dark:text-neutral-900" />
+        </div>
+    );
+}
